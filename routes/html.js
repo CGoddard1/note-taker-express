f3289e815e929f33e8da674f30f9a9bb3a18ac45
CGoddard1 <goddard.cole@gmail.com>
2023-04-18 12:53:55 -0400

    working product

--- routes/html.js
+++ routes/html.js
@@ -8,14 +8,14 @@ router.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '../public/index.html'))
 );
 
-// Gets everything and displays it
-router.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '../public/index.html'))
-);
-
 // Gets the notes page and displays it
 router.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '../public/notes.html'))
+);
+
+// Gets everything and displays it
+router.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '../public/index.html'))
 );
 
 // Exports router to be used on a different page
